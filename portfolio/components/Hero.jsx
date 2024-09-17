49e70f8d8f19c714b7012a96c66081362604900b
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-09-17 11:18:16 -0300

    feat[Hero]: update information

--- portfolio/components/Hero.jsx
+++ portfolio/components/Hero.jsx
@@ -18,28 +18,32 @@ const Hero = () => {
   return (
     <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
       <div className='container mx-auto'>
-        <div className='flex justify-between gap-x-8'>
-          {/* text */}
-          <div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
-            <div className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]'>
-              Web Developer
+        <div className='flex justify-center gap-x-8'>
+          <div className='flex max-w-[600px] flex-col justify-center items-center mx-auto xl:mx-0 text-center xl:text-left'>
+            <div className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px] text-center'>
+              FULL STACK DEVELOPER
             </div>
-            <h1 className='h1 mb-4'>Hello, my name is Dhomini Rabelo</h1>
-            <p className='subtitle max-w-[490px] mx-auto xl:mx-0'>
-              Brief description with insights into myself, my vocational
-              journey, and what I engage in professionally.
+            <h1 className='h1 mb-4 text-center'>Hello, my name is Dhomini Rabelo</h1>
+            <p className='subtitle max-w-[490px] mx-auto xl:mx-0 text-center w-full'>
+              I'm a Brazilian Full Stack Developer with a huge passion and experience in big companies. I love to learn new subjects and technologies,
+              and I'm always looking for new challenges.
             </p>
-            {/* buttons */}
             <div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'>
               <Link href='/contact'>
                 <Button className='gap-x-2'>
                   Contact me <Send size={18} />
                 </Button>
               </Link>
-              <Button variant='secondary' className='gap-x-2'>
-                Download CV
-                <Download size={18} />
-              </Button>
+              <Link
+                target='_blank'
+                href='/hero/resume.pdf'
+                className='flex items-center gap-x-2 text-secondary hover:text-primary transition-all'
+              >
+                <Button variant='secondary' className='gap-x-2'>
+                  Download CV
+                  <Download size={18} />
+                </Button>
+              </Link>
             </div>
             {/* socials */}
             <Socials
@@ -47,16 +51,13 @@ const Hero = () => {
               iconsStyles='text-foreground text-[22px] hover:text-primary transition-all'
             />
           </div>
-          {/* image */}
-          <div className='hidden xl:flex relative'>
-            {/* badge 1 */}
+          {/* <div className='hidden xl:flex relative'>
             <Badge
               containerStyles='absolute top-[24%] -left-[5rem]'
               icon={<RiBriefcase4Fill />}
               endCountNum={3}
               badgeText='Years Of Experience'
             />
-            {/* badge 2 */}
             <Badge
               containerStyles='absolute top-[80%] -left-[1rem]'
               icon={<RiTodoFill />}
@@ -64,7 +65,6 @@ const Hero = () => {
               endCountText='k'
               badgeText='Finished Projects'
             />
-            {/* badge 3 */}
             <Badge
               containerStyles='absolute top-[55%] -right-8'
               icon={<RiTeamFill />}
@@ -77,7 +77,7 @@ const Hero = () => {
               containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom'
               imgSrc='/hero/developer.png'
             />
-          </div>
+          </div> */}
         </div>
         {/* icon */}
         <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce'>
