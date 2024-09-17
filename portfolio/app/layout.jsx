50cdc9e35dc2eb91818da9097f05280aa77c73ca
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-09-17 11:26:35 -0300

    feat[Layout]: add titles and favicon

--- portfolio/app/layout.jsx
+++ portfolio/app/layout.jsx
@@ -17,6 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' suppressHydrationWarning>
+      <header>
+        <link rel='icon' href='/briefcase.svg' type='image/svg+xml' />
+        <title>Dhomini Rabelo CV</title>
+      </header>
       <body className={outfit.className}>
         <ThemeProvider attribute='class' defaultTheme='light'>
           <Header />
