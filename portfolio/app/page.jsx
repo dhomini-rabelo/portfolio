89d4dd6ff0470f16585a4bb3e1f5e10104d14380
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2024-09-17 15:48:18 -0300

    use contact component

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -1,9 +1,8 @@
 // components
 import About from '@/components/About';
+import Contact from '@/components/Contact';
 import Hero from '@/components/Hero';
-import Reviews from '@/components/Reviews';
 import Projects from '@/components/Projects';
-import Cta from '@/components/Cta';
 
 export default function Home() {
   return (
@@ -14,7 +13,7 @@ export default function Home() {
       {/* <Work /> */}
       <Projects />
       {/* <Reviews /> */}
-      <Cta />
+      <Contact />
     </main>
   );
 }
